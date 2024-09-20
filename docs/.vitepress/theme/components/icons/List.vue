58944d8fd91ff0426b1a72f217083481c6ebe3e0
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-09-20 11:11:49 +0300

    make list icons

--- docs/.vitepress/theme/components/icons/List.vue
+++ docs/.vitepress/theme/components/icons/List.vue
@@ -1,49 +1,108 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, onMounted, watch } from 'vue'
 import { B24Icon } from '@bitrix24/icons-vue'
 import type { IconRow } from '../../types'
+import { useElementSize, useEventListener, useVirtualList } from '@vueuse/core'
 import useDynamicFilter from '../../composables/useDynamicFilter'
+import splitIntoChunks from "../../utils/splitIntoChunks"
+import useSearchInput from "../../composables/useSearchInput";
+
+const ICON_SIZE = 110;
+const ICON_GRID_GAP = 24;
 
 const props = defineProps<{
-	icons: IconRow[];
+	icons: IconRow[]
 }>()
 
-const searchQuery = ref('')
+const containerRef = ref<HTMLElement | null>(null)
+const { width: containerWidth } = useElementSize(containerRef)
 
-const iconList = computed(() => {
-	return props.icons;
+const columnSize = computed(() => {
+	return Math.floor(
+		containerWidth.value / (ICON_SIZE + ICON_GRID_GAP)
+	)
 })
 
-const filteredIcons = useDynamicFilter(searchQuery, iconList, [
-	{ name: 'name' },
-	{ name: 'type' },
-])
+const iconList = computed(() => {
+	return props.icons
+})
+
+const { searchInput, searchQuery, searchQueryDebounced } = useSearchInput();
+watch(searchQueryDebounced, () => {
+	scrollTo(0)
+})
+
+const filteredIcons = useDynamicFilter(
+	searchQuery,
+	iconList,
+	[
+		{ name: 'name' },
+		{ name: 'type' }
+	]
+)
+
+const chunkedItems = computed(() => {
+	return splitIntoChunks(
+		filteredIcons.value, columnSize.value
+	)
+})
+
+const { list, containerProps, wrapperProps, scrollTo } = useVirtualList(
+	chunkedItems,
+	{
+		itemHeight: ICON_SIZE + ICON_GRID_GAP,
+		overscan: 10
+	}
+)
+
+onMounted(() => {
+	containerProps.ref.value = document.documentElement;
+	useEventListener(window, 'scroll', containerProps.onScroll)
+})
 
 </script>
 
 <template>
-	
-	<div class="mb-3 border border-gray-500 p-2">
-		<input
-			type="search"
-			v-model="searchQuery"
-			placeholder="Search query..."
-			class="flex w-full"
-		/>
-	</div>
-	<div
-		class="w-full grid gap-5 grid-cols-[repeat(auto-fill,_minmax(56px,_1fr))]"
-	>
-		<div
-			v-for="(icon, indexRow) in filteredIcons"
-			:key="icon.code"
-			class="group/item aspect-square relative flex flex-col flex-nowrap justify-center items-center cursor-pointer rounded-2xs bg-gray-30 hover:bg-gray-200 dark:bg-base-master dark:text-gray-150"
-		>
-			<B24Icon
-				:name="icon.name"
-				class="size-10"
-			/>
-			<div class="hidden z-30 group-hover/item:absolute group-hover/item:block group-hover/item:-bottom-6 bg-info-background border border-info-background-on text-info-background-on text-xs p-1.5 rounded-2xs">{{ icon.name }}</div>
+	<div ref="containerRef" class="pb-[400px]">
+		<div class="sticky z-10 top-12 pt-6 min-[960px]:pt-8 min-[960px]:top-16 -mt-8 mb-8 w-full flex bg-white shadow-[0_16px_24px_rgb(255,255,255)] dark:bg-[#0f172a] dark:shadow-[0_16px_24px_rgb(15,23,42)]">
+			<div class="relative bg-gray-30 w-full rounded text-base-master dark:text-gray-200 dark:bg-[#0f172a]">
+				<B24Icon name="Main::Search2Icon" class="absolute size-8 z-10 top-3 left-3 pointer-events-none dark:text-gray-200" />
+				<input
+					type="search"
+					v-model="searchQuery"
+					placeholder="Search icons ..."
+					class="relative justify-start rounded w-full h-14 text-lg placeholder:text-base-500 pl-12 py-2 pr-2 border hover:border-info active:border-info-background-on focus-visible:border-info-background-on"
+				/>
+			</div>
+		</div>
+		<div v-bind="wrapperProps" class="aspect-square">
+			<template
+				v-for="{ index, data: icons } in list"
+				:key="index"
+			>
+				<div class="grid grid-cols-[repeat(auto-fill,minmax(110px,1fr))] gap-6">
+					<div
+						v-for="icon in icons"
+						:key="icon.code"
+						class="group"
+					>
+						<div class="relative h-[100px] w-full">
+							<button class="absolute inset-0 flex h-full w-full cursor-auto items-center justify-center rounded text-gray-900 dark:text-gray-200 ring-1 ring-inset ring-gray-900/[0.08] dark:ring-gray-150/[0.28]">
+								<span>
+									<B24Icon
+										:name="icon.name"
+										class="size-10"
+									/>
+								</span>
+							</button>
+						</div>
+						<p
+							class="mt-3 h-10 truncate text-center text-[0.8125rem] leading-5 text-gray-500 group-focus-within:line-clamp-2 group-focus-within:whitespace-normal group-hover:line-clamp-2 group-hover:whitespace-normal"
+						>{{ icon.name }}</p>
+					</div>
+				</div>
+			</template>
+			
 		</div>
 	</div>
 </template>
