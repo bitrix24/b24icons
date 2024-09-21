1288984bf9889d7bc4bb576d5e275c872ad15f5f
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-09-21 10:33:13 +0300

    config project

--- docs/.vitepress/theme/types.ts
+++ docs/.vitepress/theme/types.ts
@@ -1,6 +1,12 @@
 export type IconRow = {
 	code: string,
+	fullCode: string,
 	name: string,
 	type: string,
-	icon: string
+	icon: string,
+	specialized?: {
+		animateSpin?: boolean,
+		width?: 'auto'|'w-lg'|'w-[21px]',
+		height?: 'auto'|'h-lg'|'h-[21px]',
+	}
 };
