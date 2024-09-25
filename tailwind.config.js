5b6bc7db65dc00e07a010db7c2da1495e21ada97
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-09-25 12:12:44 +0300

    add some plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import svgToDataUri from 'mini-svg-data-uri'
+import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette'
 
 export default {
 	important: true,
@@ -15,5 +16,23 @@ export default {
 			logs: false,
 			useLocalFonts: false
 		}),
+		function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'bg-grid': (value) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+						)}")`,
+					}),
+				},
+				{ values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+			)
+			matchUtilities(
+				{
+					highlight: (value) => ({ boxShadow: `inset 0 1px 0 0 ${value}` }),
+				},
+				{ values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+			)
+		},
 	]
 }
