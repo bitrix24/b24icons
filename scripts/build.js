7824a086d5b2119ee5aa28fa5802fdb6fe8ffbb8
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-06-02 07:01:59 +0300

    chore: improve

--- scripts/build.js
+++ scripts/build.js
@@ -1,48 +1,45 @@
 import fs from 'fs/promises';
 import camelcase from 'camelcase';
-import {rimraf} from 'rimraf';
+import { rimraf } from 'rimraf';
 import svgr from '@svgr/core';
 import * as babel from '@babel/core';
-import {compile as compileVue} from '@vue/compiler-dom';
-import {dirname} from 'path';
-import {deprecated} from './deprecated.js';
-import {typeList} from './type-list.js';
+import { compile as compileVue } from '@vue/compiler-dom';
+import { dirname } from 'path';
+import { deprecated } from './deprecated.js';
+import { typeList } from './type-list.js';
 
 let transform = {
-  '@bitrix24-icons-react': async(svg, componentName, format, isDeprecated) =>
-  {
+  '@bitrix24-icons-react': async (svg, componentName, format, isDeprecated) => {
     let component = await svgr(
       svg,
       {
         ref: true,
         titleProp: true
       },
-      {componentName}
+      { componentName }
     );
 
-    let {code} = await babel.transformAsync(
+    let { code } = await babel.transformAsync(
       component,
       {
         plugins: [
           [
             require('@babel/plugin-transform-react-jsx'),
-            {useBuiltIns: true}
+            { useBuiltIns: true }
           ]
         ],
       }
     );
 
     // Add a deprecation warning to the component
-    if(isDeprecated)
-    {
+    if (isDeprecated) {
       /** @type {string[]} */
       let lines = code.split('\n');
       lines.splice(1, 0, `/** @deprecated */`);
       code = lines.join('\n');
     }
 
-    if(format === 'esm')
-    {
+    if (format === 'esm') {
       return code
     }
 
@@ -56,21 +53,19 @@ let transform = {
         'module.exports ='
       );
   },
-  '@bitrix24-icons-vue': (svg, componentName, format, isDeprecated) =>
-  {
+  '@bitrix24-icons-vue': (svg, componentName, format, isDeprecated) => {
 
     let isHasTagStyle = false;
-    if(svg.search('<style>') !== -1)
-    {
+    if (svg.search('<style>') !== -1) {
       /**
        * let's encode the tag <style>
        */
       isHasTagStyle = true;
-      console.warn(`ⓘ component [ ${componentName} ] svg has tag [style]`);
+      console.warn(`ⓘ component [ ${ componentName } ] svg has tag [style]`);
       svg = svg.replaceAll('<style>', '<main>').replaceAll('</style>', '</main>');
     }
 
-    let {code} = compileVue(
+    let { code } = compileVue(
       svg,
       {
         mode: 'module'
@@ -80,8 +75,7 @@ let transform = {
     /**
      * @memo Add a deprecation warning to the component
      */
-    if(isDeprecated)
-    {
+    if (isDeprecated) {
       /** @type {string[]} */
       let lines = code.split('\n');
       lines.splice(2, 0, `/** @deprecated */`);
@@ -91,13 +85,11 @@ let transform = {
     /**
      * @memo let's bring back the tag <style>
      */
-    if(isHasTagStyle)
-    {
+    if (isHasTagStyle) {
       code = code.replace('main', 'style');
     }
 
-    if(format === 'esm')
-    {
+    if (format === 'esm') {
       return code.replace(
         'export function',
         'export default function'
@@ -107,14 +99,13 @@ let transform = {
     return code
       .replace(
         /import\s+\{\s*([^}]+)\s*\}\s+from\s+(['"])(.*?)\2/,
-        (_match, imports, _quote, mod) =>
-        {
+        (_match, imports, _quote, mod) => {
           let newImports = imports
             .split(',')
             .map((i) => i.trim().replace(/\s+as\s+/, ': '))
             .join(', ')
 
-          return `const { ${newImports} } = require("${mod}")`
+          return `const { ${ newImports } } = require("${ mod }")`
         }
       )
       .replace(
@@ -124,13 +115,11 @@ let transform = {
   }
 }
 
-function toUpperFirstChar(value)
-{
+function toUpperFirstChar(value) {
   return value.charAt(0).toUpperCase() + value.slice(1)
 }
 
-function prepareIconName(type, fileName)
-{
+function prepareIconName(type, fileName) {
   let componentName = camelcase(
     fileName.replace(/\.svg$/, '')
       //.replaceAll('-', '')
@@ -151,24 +140,21 @@ function prepareIconName(type, fileName)
     .replace('White', '').replace('Black', '')
     .replace('Light', '').replace('Dark', '');
 
-  if(type === 'common-service')
-  {
+  if (type === 'common-service') {
     componentName = componentName.replace('Service', '');
   }
 
   componentName = toUpperFirstChar(componentName);
 
-  return `${componentName}Icon`;
+  return `${ componentName }Icon`;
 }
 
-async function getIcons(type)
-{
-  let files = await fs.readdir(`./optimized/${type}`);
+async function getIcons(type) {
+  let files = await fs.readdir(`./optimized/${ type }`);
   return Promise.all(
-    files.map(async(file) =>
-    {
+    files.map(async (file) => {
       return {
-        svg: await fs.readFile(`./optimized/${type}/${file}`, 'utf8'),
+        svg: await fs.readFile(`./optimized/${ type }/${ file }`, 'utf8'),
         componentName: prepareIconName(type, file),
         isDeprecated: deprecated.includes(file),
       };
@@ -186,17 +172,14 @@ function exportAll(
   icons,
   format,
   includeExtension = true
-)
-{
+) {
   return icons
-    .map(({componentName}) =>
-    {
+    .map(({ componentName }) => {
       let extension = includeExtension ? '.js' : '';
-      if(format === 'esm')
-      {
-        return `export { default as ${componentName} } from './${componentName}${extension}'`;
+      if (format === 'esm') {
+        return `export { default as ${ componentName } } from './${ componentName }${ extension }'`;
       }
-      return `module.exports.${componentName} = require("./${componentName}${extension}")`;
+      return `module.exports.${ componentName } = require("./${ componentName }${ extension }")`;
     })
     .join('\n');
 }
@@ -209,9 +192,8 @@ function exportAll(
 async function ensureWrite(
   file,
   text
-)
-{
-  await fs.mkdir(dirname(file), {recursive: true})
+) {
+  await fs.mkdir(dirname(file), { recursive: true })
   await fs.writeFile(file, text, 'utf8')
 }
 
@@ -225,8 +207,7 @@ async function ensureWriteJson(
   file,
   json,
   isMinify = false
-)
-{
+) {
   await ensureWrite(
     file,
     isMinify
@@ -235,11 +216,9 @@ async function ensureWriteJson(
   )
 }
 
-async function buildIcons(pack, type, format)
-{
-  let outDir = `./packages/${pack}/dist/${type}`;
-  if(format === 'esm')
-  {
+async function buildIcons(pack, type, format) {
+  let outDir = `./packages/${ pack }/dist/${ type }`;
+  if (format === 'esm') {
     outDir += '/esm';
   }
 
@@ -249,8 +228,7 @@ async function buildIcons(pack, type, format)
   }
 
   await Promise.all(
-    icons.flatMap(async({componentName, svg, isDeprecated}) =>
-    {
+    icons.flatMap(async ({ componentName, svg, isDeprecated }) => {
       svg = svg
         //.replaceAll('none', 'currentColor')
         .replaceAll('#fff', 'currentColor')
@@ -292,38 +270,31 @@ async function buildIcons(pack, type, format)
       /** @type {string[]} */
       let types = [];
 
-      if(pack === '@bitrix24-icons-react')
-      {
+      if (pack === '@bitrix24-icons-react') {
         types.push(`import * as React from 'react';`);
-        if(isDeprecated)
-        {
+        if (isDeprecated) {
           types.push(`/** @deprecated */`);
         }
 
-        types.push(`declare const ${componentName}: React.ForwardRefExoticComponent<React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & { title?: string, titleId?: string } & React.RefAttributes<SVGSVGElement>>;`);
-        types.push(`export default ${componentName};`)
-      }
-      else
-      {
+        types.push(`declare const ${ componentName }: React.ForwardRefExoticComponent<React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & { title?: string, titleId?: string } & React.RefAttributes<SVGSVGElement>>;`);
+        types.push(`export default ${ componentName };`)
+      } else {
         types.push(`import type { FunctionalComponent, HTMLAttributes, VNodeProps } from 'vue';`);
-        if(isDeprecated)
-        {
+        if (isDeprecated) {
           types.push(`/** @deprecated */`);
         }
 
-        types.push(`declare const ${componentName}: FunctionalComponent<HTMLAttributes & VNodeProps>;`);
-        types.push(`export default ${componentName};`);
+        types.push(`declare const ${ componentName }: FunctionalComponent<HTMLAttributes & VNodeProps>;`);
+        types.push(`export default ${ componentName };`);
       }
 
       let metaDataJson;
-      try
-      {
+      try {
         metaDataJson = JSON.parse(await fs.readFile(
-          `./src/metadata/${type}/${componentName}.json`,
+          `./src/metadata/${ type }/${ componentName }.json`,
           'utf8'
         ));
-      }catch(error)
-      {
+      } catch (error) {
         metaDataJson = {
           specialized: true,
           category: null,
@@ -333,24 +304,24 @@ async function buildIcons(pack, type, format)
 
         console.error(
           error,
-          `./src/metadata/${type}/${componentName}.json`
+          `./src/metadata/${ type }/${ componentName }.json`
         );
 
         process.exit(1)
 
       }
 
-      metaDataPackageJson.icons[componentName] = {...metaDataJson};
+      metaDataPackageJson.icons[componentName] = { ...metaDataJson };
 
       return [
         await ensureWrite(
-          `${outDir}/${componentName}.js`,
+          `${ outDir }/${ componentName }.js`,
           content
         ),
         ...(types
             ? [
               await ensureWrite(
-                `${outDir}/${componentName}.d.ts`,
+                `${ outDir }/${ componentName }.d.ts`,
                 types.join('\n') + '\n'
               )
             ]
@@ -361,17 +332,17 @@ async function buildIcons(pack, type, format)
   )
 
   await ensureWrite(
-    `${outDir}/index.js`,
+    `${ outDir }/index.js`,
     exportAll(icons, format)
   );
 
   await ensureWrite(
-    `${outDir}/index.d.ts`,
+    `${ outDir }/index.d.ts`,
     exportAll(icons, 'esm', false)
   );
 
   await ensureWriteJson(
-    `${outDir}/metadata.json`,
+    `${ outDir }/metadata.json`,
     metaDataPackageJson,
     true
   );
@@ -380,8 +351,7 @@ async function buildIcons(pack, type, format)
 /**
  * @param {string[]} styles
  */
-async function buildExports(styles)
-{
+async function buildExports(styles) {
   let pkg = {}
 
   // To appease Vite's optimizeDeps feature which requires a root-level import
@@ -392,10 +362,10 @@ async function buildExports(styles)
   }
 
   // For those that want to read the version from package.json
-  pkg[`./package.json`] = {default: './package.json'}
+  pkg[`./package.json`] = { default: './package.json' }
 
-  pkg[`./metadata.json`] = {default: './dist/metadata.json'}
-  pkg[`./info-metadata.json`] = {default: './dist/info-metadata.json'}
+  pkg[`./metadata.json`] = { default: './dist/metadata.json' }
+  pkg[`./info-metadata.json`] = { default: './dist/info-metadata.json' }
 
   // For components ////
   pkg[`./components/*`] = {
@@ -408,24 +378,23 @@ async function buildExports(styles)
   }
 
   // Explicit exports for each style:
-  for(let style of styles)
-  {
-    pkg[`./${style}`] = {
-      types: `./dist/${style}/index.d.ts`,
-      import: `./dist/${style}/esm/index.js`,
-      require: `./dist/${style}/index.js`,
+  for (let style of styles) {
+    pkg[`./${ style }`] = {
+      types: `./dist/${ style }/index.d.ts`,
+      import: `./dist/${ style }/esm/index.js`,
+      require: `./dist/${ style }/index.js`,
     }
 
-    pkg[`./${style}/*`] = {
-      types: `./dist/${style}/esm/*.d.ts`,
-      import: `./dist/${style}/esm/*`,
-      require: `./dist/${style}/*`,
+    pkg[`./${ style }/*`] = {
+      types: `./dist/${ style }/esm/*.d.ts`,
+      import: `./dist/${ style }/esm/*`,
+      require: `./dist/${ style }/*`,
     }
 
-    pkg[`./${style}/*.js`] = {
-      types: `./dist/${style}/*.d.ts`,
-      import: `./dist/${style}/esm/*.js`,
-      require: `./dist/${style}/*.js`,
+    pkg[`./${ style }/*.js`] = {
+      types: `./dist/${ style }/*.d.ts`,
+      import: `./dist/${ style }/esm/*.js`,
+      require: `./dist/${ style }/*.js`,
     }
   }
 
@@ -438,23 +407,21 @@ async function buildExports(styles)
  */
 async function main(
   pack
-)
-{
-  console.log(`Building ${pack} package ...`);
+) {
+  console.log(`Building ${ pack } package ...`);
 
   // region clear ////
   await Promise.all(
-    typeList.map((type) => rimraf(`./export/${pack}/${type}/*`, {glob: true}))
+    typeList.map((type) => rimraf(`./export/${ pack }/${ type }/*`, { glob: true }))
   );
   // endregion ////
 
   // region build icons ////
-  const cjsPackageJson = {module: './esm/index.js', sideEffects: false}
-  const esmPackageJson = {type: 'module', sideEffects: false}
+  const cjsPackageJson = { module: './esm/index.js', sideEffects: false }
+  const esmPackageJson = { type: 'module', sideEffects: false }
 
   await Promise.all([
-    ...(typeList.map(async(type) =>
-    {
+    ...(typeList.map(async (type) => {
       return [
         await buildIcons(
           pack,
@@ -468,11 +435,10 @@ async function main(
         ),
       ];
     })),
-    ...(typeList.map((type) =>
-    {
+    ...(typeList.map((type) => {
       return [
-        ensureWriteJson(`./packages/${pack}/dist/${type}/esm/package.json`, esmPackageJson),
-        ensureWriteJson(`./packages/${pack}/dist/${type}/package.json`, cjsPackageJson),
+        ensureWriteJson(`./packages/${ pack }/dist/${ type }/esm/package.json`, esmPackageJson),
+        ensureWriteJson(`./packages/${ pack }/dist/${ type }/package.json`, cjsPackageJson),
       ];
     }))
   ])
@@ -490,8 +456,7 @@ async function main(
     list: []
   }
 
-  for(const type of typeList)
-  {
+  for (const type of typeList) {
     const typeName = toUpperFirstChar(camelcase(type))
 
     const typeRow = {
@@ -500,8 +465,8 @@ async function main(
       list: []
     }
 
-    const iconMetaDataFileEsm = `./packages/${pack}/dist/${type}/esm/metadata.json`;
-    const iconMetaDataFile = `./packages/${pack}/dist/${type}/metadata.json`;
+    const iconMetaDataFileEsm = `./packages/${ pack }/dist/${ type }/esm/metadata.json`;
+    const iconMetaDataFile = `./packages/${ pack }/dist/${ type }/metadata.json`;
 
     const metaDataPackageJson = JSON.parse(await fs.readFile(
       iconMetaDataFileEsm,
@@ -510,9 +475,8 @@ async function main(
 
     const iconsKey = Object.keys(metaDataPackageJson?.icons || {})
 
-    for(const icon of iconsKey)
-    {
-      const iconCode = `${typeRow.code}::${icon}`
+    for (const icon of iconsKey) {
+      const iconCode = `${ typeRow.code }::${ icon }`
 
       const iconName = metaDataPackageJson?.icons[icon]?.name
         ? metaDataPackageJson.icons[icon].name
@@ -541,55 +505,53 @@ async function main(
 
     metaDataInfoJson.list.push(typeRow)
 
-    await rimraf(iconMetaDataFile, {glob: true})
-    await rimraf(iconMetaDataFileEsm, {glob: true})
+    await rimraf(iconMetaDataFile, { glob: true })
+    await rimraf(iconMetaDataFileEsm, { glob: true })
   }
 
   await ensureWriteJson(
-    `./packages/${pack}/dist/metadata.json`,
+    `./packages/${ pack }/dist/metadata.json`,
     metaDataJson,
     true
   )
 
   await ensureWriteJson(
-    `./packages/${pack}/dist/info-metadata.json`,
+    `./packages/${ pack }/dist/info-metadata.json`,
     metaDataInfoJson,
     true
   )
 
   await ensureWriteJson(
-    `./packages/${pack}/src/metadata.json`,
+    `./packages/${ pack }/src/metadata.json`,
     metaDataJson
   )
 
   await ensureWriteJson(
-    `./packages/${pack}/src/info-metadata.json`,
+    `./packages/${ pack }/src/info-metadata.json`,
     metaDataInfoJson
   )
   // endregion ////
 
   // region VUE.component ////
-  if(pack === '@bitrix24-icons-vue')
-  {
+  if (pack === '@bitrix24-icons-vue') {
     console.log(``)
     console.log(`Init component ...`)
 
     const regex = /(\/\/ #CASE_RENDER_START# \/\/\/)([\s\S]*?)(\/\/ #CASE_RENDER_STOP# \/\/\/)/
-    const componentPath = `./packages/${pack}/src/components/B24Icon.ts`
+    const componentPath = `./packages/${ pack }/src/components/B24Icon.ts`
 
     let componentData = await fs.readFile(
       componentPath,
       'utf8'
     );
 
-    const caseRender = metaDataJson.list.sort().map((code) =>
-    {
+    const caseRender = metaDataJson.list.sort().map((code) => {
       const tmp = code.split('::')
 
-      return `case "${code}": return defineAsyncComponent(() => { return import('../../dist/${tmp[0]}/esm/${tmp[1]}.js') })`
+      return `case "${ code }": return defineAsyncComponent(() => { return import('../../dist/${ tmp[0] }/esm/${ tmp[1] }.js') })`
     })
 
-    componentData = componentData.replace(regex, `$1\n${caseRender.join("\n")}\n$3`)
+    componentData = componentData.replace(regex, `$1\n${ caseRender.join("\n") }\n$3`)
 
     await ensureWrite(
       componentPath,
@@ -600,30 +562,28 @@ async function main(
 
   // region package.json ////
   let packageJson = JSON.parse(await fs.readFile(
-    `./packages/${pack}/package.json`,
+    `./packages/${ pack }/package.json`,
     'utf8'
   ))
 
   packageJson.exports = await buildExports(typeList)
 
   await ensureWriteJson(
-    `./packages/${pack}/package.json`,
+    `./packages/${ pack }/package.json`,
     packageJson
   )
   // endregion ////
 
-  return console.log(`Finished building ${pack} package.`)
+  return console.log(`Finished building ${ pack } package.`)
 }
 
 let [pack] = process.argv.slice(2);
 
-if(!pack)
-{
+if (!pack) {
   throw new Error('Please specify a package')
 }
 
 main(pack)
-  .catch((error) =>
-  {
+  .catch((error) => {
     console.error(error);
   })
