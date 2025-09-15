1a04ade4457210f8093b8b0b648a8c85597cc3eb
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-09-15 07:45:25 +0300

    chore(ui): improve

--- docs/.vitepress/theme/components/ui/ButtonMenu.vue
+++ docs/.vitepress/theme/components/ui/ButtonMenu.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { useStorage } from '@vueuse/core'
 import ChevronDownIcon from '@bitrix24/b24icons-vue/actions/ChevronDownIcon'
 
