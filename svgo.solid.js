9aaedfe4d3b9804e69baea0cac880a84a924de81
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-11-12 08:14:20 +0300

    fix(svgo): improve style id

--- svgo.solid.js
+++ svgo.solid.js
@@ -5,6 +5,21 @@ export default {
     'sortAttrs',
     'cleanupListOfValues',
     {
+      name: 'prefixIds',
+      params: {
+        prefix: (_svg, info) => {
+          const normalizedPath = info.path.replace(/\\/g, '/')
+          const pathParts = normalizedPath.split('/')
+          const lastParts = pathParts.slice(-2)
+          return lastParts
+            .map(part => part.replace('.svg', ''))
+            .join('_')
+            .replace(/[^\w-]/g, '_')
+        },
+        prefixSeparator: '_'
+      }
+    },
+    {
       name: 'addAttributesToSVGElement',
       params: {
         attributes: [
